5d925a88d32b88c096003031a8efee5f36f1dd13
Zhornovy <ed@zhornovy.com>
2017-04-21 14:48:46 +0300

    Added assert & record
    Added two different approaches

--- Database.pl
+++ Database.pl
@@ -1,23 +1,38 @@
 :- dynamic point/3.
 %Database filling
 db_filling :-
-assert(point(p1, 1, 1)),
-assert(point(p2, 1, 2)),
-assert(point(p3, 1, 3)),
-assert(point(p4, 1, 4)),
-assert(point(p5, 1, 5)).
-% +Radius, -Result List
+ assert(point(p1, 1, 1)),
+ assert(point(p2, 1, 2)),
+ assert(point(p3, 1, 3)),
+ assert(point(p4, 1, 4)),
+ assert(point(p5, 1, 5)).
+db_filling2 :-
+ recordz(point,(p1, 1, 1)),
+ recordz(point,(p2, 1, 2)),
+ recordz(point,(p3, 1, 3)),
+ recordz(point,(p4, 1, 4)),
+ recordz(point,(p5, 1, 5)).
 
+%+Radius, -Result
 main(Radius, Result) :-
-db_filling, setof(N, findIntersections(N, Radius), Result).
+ db_filling, setof(N, find(N, Radius), Result).
+main2(Radius, Result) :-
+ db_filling2, setof(N, find2(N, Radius), Result).
 
-% -Centers and points count, +Radius
-findIntersections([C1, C2], Radius) :-
-point(C1, X1, Y1),
-point(C2, X2, Y2),
-C1 @< C2,
-sqrt(((X2-X1)*(X2-X1))+((Y2-Y1)*(Y2-Y1))) > Radius*2.
+% -Pair of Points, +Radius
+find([C1, C2], Radius) :-
+ point(C1, X1, Y1),
+ point(C2, X2, Y2),
+ C1 @< C2,
+ sqrt(((X2-X1)*(X2-X1))+((Y2-Y1)*(Y2-Y1))) > Radius*2.
+find2([C1, C2], Radius) :-
+ recorded(point,(C1, X1, Y1)),
+ recorded(point,(C2, X2, Y2)),
+ C1 @< C2,
+ sqrt(((X2-X1)*(X2-X1))+((Y2-Y1)*(Y2-Y1))) > Radius*2.
 
+
+%+First List,-New List
 remove_duplicates([],[]).
 remove_duplicates([H | T], List) :-
      member(H, T),
